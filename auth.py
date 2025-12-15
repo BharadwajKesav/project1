96f91b417fec17a6fc02efe4cf8af4979885b7fc
Bharadwaj Kesav P <am.en.u4cse22316@am.students.amrita.edu>
2025-12-15 20:59:24 +0530

    Delete auth.py

--- auth.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-
-def hash_password(password):
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-
-def verify_password(stored_hash, password):
-    return bcrypt.checkpw(password.encode(), stored_hash)
